12278839bd58e4bbf7ae5b47682241ea427507b5
Greg <402164923@qq.com>
2022-11-08 15:27:01 +0800

    vuex持久化以及axios的二次封装

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,22 @@
 import { createStore } from 'vuex'
+// 引入持久化插件
+import createPersistedstate from 'vuex-persistedstate'
+
+import cat from './modules/cat'
+import category from './modules/category'
+import user from './modules/user'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    cat,
+    category,
+    user
+  },
+  // 配置插件
+  plugins:[createPersistedstate({
+    // 本地存储的名字
+    key:'eRabbit-client-pc-124-store',
+    // 指定需要存储的模块
+    paths:['user','cart']
+  })]
 })
---
+++ src/store/modules/category.js
@@ -0,0 +1,10 @@
+// 分类
+export default {
+    namespaced:true,
+    state(){
+        return {
+            // 分类信息集合
+            list:[]
+        }
+    }
+}
